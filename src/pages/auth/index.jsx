ca0b081632a1c540cb2563be40acbe9caa023f68
barnalixd <dasbarnali690@gcekbpatna.ac.in>
2025-02-10 14:26:42 +0530

    46:25

--- src/pages/auth/index.jsx
+++ src/pages/auth/index.jsx
@@ -1,7 +1,17 @@
+import { TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Tabs, TabsContent } from "@radix-ui/react-tabs";
 import { GraduationCap } from "lucide-react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 function Authpage() {
+const [activeTab, setActiveTab] = useState('signin')    
+
+function handleTabChange(value){
+    setActiveTab(value)
+}
+
+
     return <div className="flex flex-col min-h-screen">
         <header className="px-4 lg:px-6 h-14 flex items-center border-b">
             <Link to={'/'} className="flex items-center justify-center">
@@ -9,6 +19,21 @@ function Authpage() {
             <span className="font-extrabold text-xl">LMS LEARN</span>
             </Link>
         </header>
+        <div className="flex items-center justify-center min-h-screen bg-background">
+            <Tabs 
+            value={activeTab}
+            defaultValue="signin"
+            onValueChange={handleTabChange}
+            className="w-full max-w-md"
+            >
+                <TabsList className="grid w-full grid-cols-2">
+                    <TabsTrigger value='signin'>Sign In</TabsTrigger>
+                    <TabsTrigger value="signup">Sign Up</TabsTrigger>
+                </TabsList>
+                <TabsContent value="signin">signin</TabsContent>
+                <TabsContent value="signup">signup</TabsContent>
+            </Tabs>
+        </div>
     </div>
 }
 
