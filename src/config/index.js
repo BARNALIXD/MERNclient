52c002d1a3cd1312c78c7724760ba84d3f069653
barnalixd <dasbarnali690@gcekbpatna.ac.in>
2025-02-11 19:10:51 +0530

    initial form data 1:06:03

--- src/config/index.js
+++ src/config/index.js
@@ -1,13 +1,13 @@
 export const signUpFormControls = [
   {
-    name: "username",
+    name: "userName",
     label: "User Name",
     placeholder: "Enter your user name",
     type: "text",
     componentType: "input",
   },
   {
-    name: "email",
+    name: "userEmail",
     label: "User Email",
     placeholder: "Enter your user email",
     type: "email",
@@ -25,7 +25,7 @@ export const signUpFormControls = [
 
 export const signInFormControls = [
   {
-    name: "email",
+    name: "userEmail",
     label: "User Email",
     placeholder: "Enter your user email",
     type: "email",
@@ -39,3 +39,14 @@ export const signInFormControls = [
     componentType: "input",
   },
 ];
+
+export const initialSignInFormData = {
+  userEmail: "",
+  password: "",
+}
+
+export const initialSignUpFormData = {
+  userName: "",
+  userEmail: "",
+  password: "",
+}
--- src/context/auth-context/index.jsx
+++ src/context/auth-context/index.jsx
@@ -1,7 +1,9 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 export const AuthContext = createContext(null);
 
 export default function AuthProvider({children}) {
+
+  const [signInFormData, setSignInFormData] = useState
   return <AuthContext.Provider value={{}}>{children}</AuthContext.Provider>
 }
