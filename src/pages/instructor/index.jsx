44c1564d4f238bf5faabd64930ab9d4fb509e272
barnalixd <dasbarnali690@gcekbpatna.ac.in>
2025-02-13 23:46:43 +0530

    menuitems 2:26:09

--- src/pages/instructor/index.jsx
+++ src/pages/instructor/index.jsx
@@ -1,7 +1,43 @@
-
+import InstructorCourses from "@/components/instructor-view/courses";
+import InstructorDashboard from "@/components/instructor-view/dashboard";
+import { BarChart, LogOut } from "lucide-react";
 
 function InstructorDashboardpage() {
-    return <div>InstructorDashboard</div>
+  const menuItems = [
+    {
+      icon: BarChart,
+      label: "Dashboard",
+      value: "dashboard",
+      component: <InstructorDashboard />,
+    },
+    {
+      icon: Book,
+      label: "Courses",
+      value: "courses",
+      component: <InstructorCourses />,
+    },
+    {
+      icon: LogOut,
+      label: "Logout",
+      value: "logout",
+      component: null,
+    },
+  ];
+
+  return (
+    <div className="flex flex-col min-h-screen bg-gray-100">
+      <aside className="w-64 bg-white shadow-md hidden md:block">
+        <div className="p-4">
+          <h2 className="text-2xl font-bold mb-4">Instructor View</h2>
+          <nav>
+            {
+                menuItems.map()
+            }
+          </nav>
+        </div>
+      </aside>
+    </div>
+  );
 }
 
 export default InstructorDashboardpage;
