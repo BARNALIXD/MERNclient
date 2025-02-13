b992663b8eeb92f80c16e5aacebf1c1c39bdb6e1
barnalixd <dasbarnali690@gcekbpatna.ac.in>
2025-02-13 23:10:35 +0530

    2:20:16

--- src/context/auth-context/index.jsx
+++ src/context/auth-context/index.jsx
@@ -1,3 +1,4 @@
+import { Skeleton } from "@/components/ui/skeleton";
 import { initialSignInFormData, initialSignUpFormData } from "@/config";
 import { checkAuthService, loginService, registerService } from "@/services";
 import { createContext, useEffect, useState } from "react";
@@ -8,70 +9,83 @@ export default function AuthProvider({ children }) {
   const [signInFormData, setSignInFormData] = useState(initialSignInFormData);
   const [signUpFormData, setSignUpFormData] = useState(initialSignUpFormData);
   const [auth, setAuth] = useState({
-    authenticate : false,
-    user : null,
-  })
+    authenticate: false,
+    user: null,
+  });
 
+  const [loading, setLoading] = useState(true);
 
-  async function handleRegisterUser(event){
+  async function handleRegisterUser(event) {
     event.preventDefault();
     const data = await registerService(signUpFormData);
-    }
-  async function handleLoginUser(event){
+  }
+  async function handleLoginUser(event) {
     event.preventDefault();
     const data = await loginService(signInFormData);
 
-    if(data.success){
-      sessionStorage.setItem('accessToken' , JSON.stringify(data.data.accessToken));
+    if (data.success) {
+      sessionStorage.setItem(
+        "accessToken",
+        JSON.stringify(data.data.accessToken)
+      );
       setAuth({
         authenticate: true,
-        user: data.data.user
+        user: data.data.user,
       });
     } else {
       setAuth({
         authenticate: false,
-        user: null
+        user: null,
       });
     }
   }
 
   //check auth user
-  async function checkAuthUser(){
-    const data = await checkAuthService();
-
-    if(data.success){
-      setAuth({
-        authenticate: true,
-        user: data.data.user
-      });
-    } else {
-      setAuth({
-        authenticate: false,
-        user: null
-      });
+  async function checkAuthUser() {
+    try {
+      const data = await checkAuthService();
+      if (data.success) {
+        setAuth({
+          authenticate: true,
+          user: data.data.user,
+        });
+        setLoading(false);
+      } else {
+        setAuth({
+          authenticate: false,
+          user: null,
+        });
+        setLoading(false);
+      }
+    } catch (error) {
+      console.log(error);
+      if(!error?.response?.data.success){
+        setAuth({
+          authenticate: false,
+          user: null,
+        });
+        setLoading(false);
+      }
     }
   }
 
-  useEffect(()=> {
+  useEffect(() => {
     checkAuthUser();
-  },[])
-
-  console.log(auth);
-
+  }, []);
 
   return (
-  <AuthContext.Provider 
-  value={{
-    signInFormData,
-    setSignInFormData,
-    signUpFormData,
-    setSignUpFormData,
-    handleRegisterUser,
-    handleLoginUser,
-    auth,
-  }}
-  >
-    {children}
+    <AuthContext.Provider
+      value={{
+        signInFormData,
+        setSignInFormData,
+        signUpFormData,
+        setSignUpFormData,
+        handleRegisterUser,
+        handleLoginUser,
+        auth,
+      }}
+    >
+      {loading ? <Skeleton /> : children}
     </AuthContext.Provider>
   );
 }
