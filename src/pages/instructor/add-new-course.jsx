17e9fabe0ae21a26df3cb3274d3efa56bad284c0
barnalixd <dasbarnali690@gcekbpatna.ac.in>
2025-02-14 12:50:26 +0530

    2:50:57

--- src/pages/instructor/add-new-course.jsx
+++ src/pages/instructor/add-new-course.jsx
@@ -1,5 +1,43 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Tabs } from "@radix-ui/react-tabs";
+
 function AddNewCoursePage() {
-  return <div>AddNewCoursePage</div>;
+  return (
+    <div className="container mx-auto p-4">
+      <div className="flex justify-between">
+        <h1 className="text-3xl font-extrabold mb-5">Create a new course</h1>
+        <Button className="text-sm tracking-wider font-bold px-8">
+          SUBMIT
+        </Button>
+      </div>
+      <Card>
+        <CardContent>
+          <div className="container mx-auto p-4">
+            <Tabs defaultValue="curriculum" className="space-y-4">
+              <TabsList>
+                <TabsTrigger value="curriculum">Curriculum</TabsTrigger>
+                <TabsTrigger value="course-landing-page">
+                  Course Landing Page
+                </TabsTrigger>
+                <TabsTrigger value="settings">Settings</TabsTrigger>
+              </TabsList>
+              <TabsContent value="curriculum">
+
+              </TabsContent>
+              <TabsContent value="course-landing-page">
+                
+                </TabsContent>
+                <TabsContent value="settings">
+                
+                </TabsContent>
+            </Tabs>
+          </div>
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
 
 export default AddNewCoursePage;
