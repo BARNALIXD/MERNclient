a70866169bd8b398827d620771e6b9fa36eb4efd
barnalixd <dasbarnali690@gcekbpatna.ac.in>
2025-02-15 11:39:39 +0530

    3:08:04

--- src/components/instructor-view/courses/add-new-course/course-curriculum.jsx
+++ src/components/instructor-view/courses/add-new-course/course-curriculum.jsx
@@ -1,5 +1,31 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { InstructorContext } from "@/context/instructor-context";
+import { useContext } from "react";
+
 function CourseCurriculum() {
-  return <div>CourseCurriculum</div>;
+  const { courseCurriculumFormData, setCourseCurriculumFormData } =
+    useContext(InstructorContext);
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Create Course Curriculum</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <Button>Add Lecture</Button>
+        <div className="mt-4 space-y-4">
+          {courseCurriculumFormData.map((curriculumItem, index)=>(
+          <div className="border p-5 rounded-md">
+            <div className="flex gap-5">
+              <h3 className="font-semibold">Lecture {index + 1}</h3>
+            </div>
+          </div>
+          ))}
+        </div>
+      </CardContent>
+    </Card>
+  );
 }
 
 export default CourseCurriculum;
--- src/config/index.js
+++ src/config/index.js
@@ -162,3 +162,13 @@ export const courseLandingInitialFormData = {
   welcomeMessage: "",
   image: "",
 };
+
+export const courseCurriculumInitialFormData = [
+  {
+    title: "",
+    videoUrl: "",
+    freePreview: false,
+    public_id: "",
+  },
+
+];
