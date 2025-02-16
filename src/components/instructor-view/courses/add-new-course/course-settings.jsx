17f6058e8b2100a71e06d7e1728d19b793316786
barnalixd <dasbarnali690@gcekbpatna.ac.in>
2025-02-16 18:21:43 +0530

    3:46:28

--- src/components/instructor-view/courses/add-new-course/course-settings.jsx
+++ src/components/instructor-view/courses/add-new-course/course-settings.jsx
@@ -1,24 +1,41 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { InstructorContext } from "@/context/instructor-context";
+import { useContext } from "react";
 
 function CourseSettings() {
-  return <Card>
-    <CardHeader>
-      <CardTitle>Course Settings</CardTitle>
-    </CardHeader>
-    <CardContent>
-      <div className="flex flex-col gap-3">
-        <Label>
-          Upload Course Image
-        </Label>
-      <Input
-      type="file"
-      accept="image/*"
-      />
-      </div>
-    </CardContent>
-  </Card>
+  const { courseLandingFormData, setCourseLandingFormData } =
+    useContext(InstructorContext);
+
+    aysnc function handleImageUploadChange(event){
+          const selectedImage = event.target.files[0];
+
+          if(selectedImage){
+            const imageFormData = new FormData();
+            imageFormData.append('file', selectedImage);
+
+            try{
+
+            }catch(e){
+              console.log(e);
+            }
+          }
+    }
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Course Settings</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <div className="flex flex-col gap-3">
+          <Label>Upload Course Image</Label>
+          <Input onChange={handleImageUploadChange}type="file" accept="image/*" />
+        </div>
+      </CardContent>
+    </Card>
+  );
 }
 
 export default CourseSettings;
