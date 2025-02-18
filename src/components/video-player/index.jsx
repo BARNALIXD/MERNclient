65a89b67f54c7d184b7d2c0457a8f64c45afd544
barnalixd <dasbarnali690@gcekbpatna.ac.in>
2025-02-18 12:32:29 +0530

    4:25

--- src/components/video-player/index.jsx
+++ src/components/video-player/index.jsx
@@ -1,5 +1,8 @@
+import { Slider } from "../ui/slider";
 import { useRef, useState } from "react";
 import ReactPlayer from "react-player";
+import { Button } from "../ui/button";
+import { Pause, Play } from "lucide-react";
 
 function VideoPlayer({ width = "100%", height = "100", url }) {
   const [playing, setPlaying] = useState(false);
@@ -14,8 +17,10 @@ function VideoPlayer({ width = "100%", height = "100", url }) {
   const playerContainerRef = useRef(null);
   const controlsTimeoutRef = useRef(null);
 
-  function handlePlayandPause() {
-    setPlaying(!playing);
+  function handlePlayandPause() {}
+
+  function handleProgress() {
+    setPlaying(true);
   }
 
   return (
@@ -25,7 +30,48 @@ function VideoPlayer({ width = "100%", height = "100", url }) {
     ${isFullScreen ? "w-screen h-screen" : ""}
     `}
       style={{ width, height }}
-    ></div>
+    >
+      <ReactPlayer
+        ref={playerRef}
+        className="absolute top-0 left-0"
+        width="100%"
+        height="100%"
+        url={url}
+        playing={playing}
+        volume={volume}
+        muted={muted}
+        onProgress={handleProgress}
+      />
+      {showControls && (
+        <div
+          className={`absolute bottom-0 left-0 right-0 bg-gray-800 bg-opacity-75 p-4 transition-opacity duration-300 ${
+            showControls ? "opacity-100" : "opacity-0"
+          } `}
+        >
+          <Slider
+          value={[played *100]}
+          max={100}
+          step={0.1}
+          onValueChange={}
+          onValueCommit={}
+          className="w-full mb-4"
+          />
+          <div className="flex items-center justify-between">
+           <div className="flex items-center space-x-2">
+            <Button variant="ghost" size="icon" onClick={handlePlayandPause}
+            className="text-white hover:text-primary hover:bg-gray-700"
+            > 
+            {
+              playing ? <Pause className="h-6 w-6"/> : <Play className="h-6 w-6"/>
+            }
+
+            </Button>
+            <Button></Button>
+           </div>
+          </div>
+        </div>
+      )}
+    </div>
   );
 }
 
