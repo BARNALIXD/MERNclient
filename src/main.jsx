ad18b49205d7c83ec12bf7a8ad50c30fb73806b4
barnalixd <dasbarnali690@gcekbpatna.ac.in>
2025-02-27 12:57:04 +0530

    6:55:21

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,15 @@ import App from "./App.jsx";
 import "./index.css";
 import AuthProvider from "./context/auth-context/index.jsx";
 import InstructorProvider from "./context/instructor-context/index.jsx";
+import StudentProvider from "./context/student-context/index.jsx";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <AuthProvider>
       <InstructorProvider>
-        <App />
+        <StudentProvider>
+          <App />
+        </StudentProvider>
       </InstructorProvider>
     </AuthProvider>
   </BrowserRouter>
