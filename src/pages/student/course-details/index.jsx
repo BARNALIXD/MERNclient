21273ac6b5713bb587a6f762334a82d3ca6323d4
barnalixd <dasbarnali690@gcekbpatna.ac.in>
2025-03-10 21:19:56 +0530

    update

--- src/pages/student/course-details/index.jsx
+++ src/pages/student/course-details/index.jsx
@@ -1,10 +1,20 @@
+import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
 import { Skeleton } from "@/components/ui/skeleton";
+import VideoPlayer from "@/components/video-player";
 import { StudentContext } from "@/context/student-context";
 import { fetchStudentViewCourseDetailsService } from "@/services";
-import { CheckCircle, Globe } from "lucide-react";
-import { useContext, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { CheckCircle, Globe, Lock, PlayCircle } from "lucide-react";
+import { useContext, useEffect, useState } from "react";
+import { useLocation, useParams } from "react-router-dom";
 
 function StudentViewCourseDetailsPage() {
   const {
@@ -15,16 +25,24 @@ function StudentViewCourseDetailsPage() {
     loadingState,
     setLoadingState,
   } = useContext(StudentContext);
+  // debugger
+  // console.log(currentCourseDetailsId);
+  const [displayCurrentVideoFreePreview, setDisplayCurrentVideoFreePreview] =
+    useState(null);
+  const [showFreePreviewDailog, setShowFreePreviewDailog] = useState(false);
 
   const { id } = useParams();
+  const location = useLocation();
 
   async function fetchStudentViewCourseDetails() {
     const response = await fetchStudentViewCourseDetailsService(
       currentCourseDetailsId
     );
+  
+   
 
     if (response?.success) {
-      setStudentViewCourseDetails(response.data);
+      setStudentViewCourseDetails(response?.data);
       setLoadingState(false);
     } else {
       setStudentViewCourseDetails(null);
@@ -32,29 +50,55 @@ function StudentViewCourseDetailsPage() {
     }
   }
 
+
+
+  function handleSetFreePreview(getCurrentVideoInfo) {
+    console.log(getCurrentVideoInfo);
+    setDisplayedCurrentVideoFreePreview(getCurrentVideoInfo?.videoUrl);
+  }
+
   useEffect(() => {
-    if (currentCourseDetailsId !== null) fetchStudentViewCourseDetails();
+    if (displayCurrentVideoFreePreview !== null) setShowFreePreviewDailog(true);
+  }, [displayCurrentVideoFreePreview]);
+
+  useEffect(() => {
+    if (currentCourseDetailsId !==null)  fetchStudentViewCourseDetails();
   }, [currentCourseDetailsId]);
 
   useEffect(() => {
     if (id) setCurrentCourseDetailsId(id);
   }, [id]);
 
+  useEffect(() => {
+    if (!location.pathname.includes("courses/details"))
+      setStudentViewCourseDetails(null), setCurrentCourseDetailsId(null);
+  }, [location.pathname]);
+
   if (loadingState) return <Skeleton />;
+
+  const getIndexOfFreePreviewUrl =
+    studentViewCourseDetails !== null
+      ? studentViewCourseDetails?.curriculum?.findIndex(
+          (item) => item.freePreview
+        )
+      : -1;
+
+  console.log(
+    getIndexOfFreePreviewUrl,
+    studentViewCourseDetails?.curriculum[getIndexOfFreePreviewUrl],
+    "barnali"
+  );
 
   return (
     <div className=" mx-auto p-4">
       <div className="bg-gray-900 text-white p-8 rounded-t-lg">
-        <h1 className="text=3xl font-bold mb-4">
+        <h1 className="text-3xl font-bold mb-4">
           {studentViewCourseDetails?.title}
         </h1>
         <p className="text-xl mb-4">{studentViewCourseDetails?.subtitle}</p>
         <div className="flex items-center space-x-4 mt-2 text-sm">
           <span>Created By {studentViewCourseDetails?.instructorName}</span>
-          <span>
-            {" "}
-            Created On {studentViewCourseDetails?.date.split("T")[0]}{" "}
-          </span>
+          <span>Created On {studentViewCourseDetails?.date.split("T")[0]}</span>
           <span className="flex items-center">
             <Globe className="mr-1 h-4 w-4" />
             {studentViewCourseDetails?.primaryLanguage}
@@ -78,7 +122,7 @@ function StudentViewCourseDetailsPage() {
                 {studentViewCourseDetails?.objectives
                   .split(",")
                   .map((objective, index) => (
-                    <li key={index} className="flex item-start">
+                    <li key={index} className="flex items-start">
                       <CheckCircle className="mr-2 h-5 w-5 text-green-500 flex-shrink-0" />
                       <span>{objective}</span>
                     </li>
@@ -86,8 +130,108 @@ function StudentViewCourseDetailsPage() {
               </ul>
             </CardContent>
           </Card>
+          <Card className="mb-8">
+            <CardHeader>
+              <CardTitle>Course Description</CardTitle>
+            </CardHeader>
+            <CardContent>{studentViewCourseDetails?.description}</CardContent>
+          </Card>
+          <Card className="mb-8">
+            <CardHeader>
+              <CardTitle>Course Curriculum</CardTitle>
+            </CardHeader>
+            <CardContent>
+              {studentViewCourseDetails?.curriculum?.map(
+                (curriculumItem, index) => (
+                  <li
+                    className={`${
+                      curriculumItem?.freePreview
+                        ? "cursor-pointer"
+                        : "cursor-not-allowed"
+                    } flex items-center mb-4`}
+                    onClick={
+                      curriculumItem?.freePreview
+                        ? () => handleSetFreePreview(curriculumItem)
+                        : null
+                    }
+                  >
+                    {curriculumItem?.freePreview ? (
+                      <PlayCircle className="mr-2 h-4 w-4" />
+                    ) : (
+                      <Lock className="mr-2 h-4 w-4" />
+                    )}
+                    <span>{curriculumItem?.title}</span>
+                  </li>
+                )
+              )}
+            </CardContent>
+          </Card>
         </main>
+        <aside className="w-full md:w-[500px]">
+          <Card className="sticky top-4">
+            <CardContent className="p-6">
+              <div className="aspect-video mb-4 rounded-lg flex items-center justify-center">
+                <VideoPlayer
+                  url={
+                    getIndexOfFreePreviewUrl !== -1
+                      ? studentViewCourseDetails?.curriculum[
+                          getIndexOfFreePreviewUrl
+                        ].videoUrl
+                      : ""
+                  }
+                  width="450px"
+                  height="200px"
+                />
+              </div>
+              <div className="mb-4">
+                <span className="text-3xl font-bold">
+                  ${studentViewCourseDetails?.pricing}
+                </span>
+              </div>
+              <Button className="w-full">Buy tommorow</Button>
+            </CardContent>
+          </Card>
+        </aside>
       </div>
+      <Dialog
+        open={showFreePreviewDailog}
+        onOpenChange={() => {
+          setShowFreePreviewDailog(false);
+          setDisplayCurrentVideoFreePreview(null);
+        }}
+      >
+        <DialogContent className="w-[800px]">
+          <DialogHeader>
+            <DialogTitle>Course Preview</DialogTitle>
+          </DialogHeader>
+          <div className="aspect-video rounded-lg flex items-center justify-center">
+            <VideoPlayer
+              url={displayCurrentVideoFreePreview}
+              width="450px"
+              height="200px"
+            />
+          </div>
+          <div className="flex flex-col gap-2">
+            {studentViewCourseDetails?.curriculum
+              ?.filter((item) => item.freePreview)
+              .map((filteredItem) => (
+                <p
+                  onClick={() => handleSetFreePreview(filteredItem)}
+                  className="cursor-pointer text-[16px] font-medium"
+                >
+                  {filteredItem?.title}
+                </p>
+              ))}
+          </div>
+          <DialogFooter className="sm:justify-start">
+            <DialogClose asChild>
+              <Button type="button" variant="secondary">
+                Close
+              </Button>
+            </DialogClose>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 }
